d986b1e6fe6dda3f5332a2b0419391bec75de3b4
Lalo <Lalo@DESKTOP-NJGC4AH.mshome.net>
2019-04-03 10:55:19 -0600

    Se agrega método para actualizar la tabla de equipos automáticamente después de realizar alguna operación de un equipo.

--- src/controller/equipos/CtlEquipo.java
+++ src/controller/equipos/CtlEquipo.java
@@ -19,94 +19,90 @@ import view.equipo.PanelEquipo;
  *
  * @author Lalo
  */
-public class CtlEquipo implements ActionListener{
-
+public class CtlEquipo implements ActionListener {
 
     private PanelEquipo pnlEquipo;
     private FrmEquipo frmEquipo;
     private BnsEquipo mdlEquipo;
 
-    
     public CtlEquipo(PanelEquipo pnlEquipo, BnsEquipo mdlEquipo) {
         this.pnlEquipo = pnlEquipo;
         this.mdlEquipo = mdlEquipo;
     }
-    
+
     public CtlEquipo(PanelEquipo pnlEquipo, FrmEquipo frmEquipo, BnsEquipo mdlEquipo) {
         this.pnlEquipo = pnlEquipo;
         this.frmEquipo = frmEquipo;
         this.mdlEquipo = mdlEquipo;
     }
-    
-    
-    
 
     @Override
     public void actionPerformed(ActionEvent e) {
-             try{
-            if(e.getActionCommand().equalsIgnoreCase("Buscar")){                                
+        try {
+            if (e.getActionCommand().equalsIgnoreCase("Buscar")) {
                 frmEquipo = new FrmEquipo();
                 frmEquipo.setControlador(new CtlEquipo(pnlEquipo, frmEquipo, mdlEquipo));
-                String busqueda = pnlEquipo.obtenerBusqueda();                                
-                List<DtoEquipo>lista = mdlEquipo.buscarFiltrando(busqueda);
-                frmEquipo.dispose();                
-                pnlEquipo.VisualizarInformacion(lista);                
-            }else if(e.getActionCommand().equalsIgnoreCase("Agregar")){
+                String busqueda = pnlEquipo.obtenerBusqueda();
+                List<DtoEquipo> lista = mdlEquipo.buscarFiltrando(busqueda);
+                frmEquipo.dispose();
+                pnlEquipo.VisualizarInformacion(lista);
+            } else if (e.getActionCommand().equalsIgnoreCase("Agregar")) {
                 frmEquipo = new FrmEquipo();
                 frmEquipo.setControlador(new CtlEquipo(pnlEquipo, frmEquipo, mdlEquipo));
                 frmEquipo.setTitle("Agregar equipo");
                 frmEquipo.setAccion("Agregar");
                 frmEquipo.setVisible(true);
-            } else if(e.getActionCommand().equalsIgnoreCase("Editar")){
+            } else if (e.getActionCommand().equalsIgnoreCase("Editar")) {
                 frmEquipo = new FrmEquipo();
                 frmEquipo.setControlador(new CtlEquipo(pnlEquipo, frmEquipo, mdlEquipo));
                 frmEquipo.setTitle("Editar equipo");
                 frmEquipo.setAccion("Editar");
-                String nombre = pnlEquipo.obtenerDatoEquipo();                
-                frmEquipo.visualizarDatos(mdlEquipo.buscarEquipo(nombre),0);                
+                String nombre = pnlEquipo.obtenerDatoEquipo();
+                frmEquipo.visualizarDatos(mdlEquipo.buscarEquipo(nombre), 0);
                 frmEquipo.setVisible(true);
-            } else if(e.getActionCommand().equalsIgnoreCase("Borrar")){
+            } else if (e.getActionCommand().equalsIgnoreCase("Borrar")) {
                 frmEquipo = new FrmEquipo();
                 frmEquipo.setControlador(new CtlEquipo(pnlEquipo, frmEquipo, mdlEquipo));
                 frmEquipo.setTitle("Borrar equipo");
                 frmEquipo.setAccion("Borrar");
                 String equipo = pnlEquipo.obtenerDatoEquipo();
-                frmEquipo.visualizarDatos(mdlEquipo.buscarEquipo(equipo),1);                
+                frmEquipo.visualizarDatos(mdlEquipo.buscarEquipo(equipo), 1);
                 frmEquipo.setVisible(true);
-            } else if(e.getActionCommand().equals("Aceptar")){
-                if(frmEquipo.getAccion().equalsIgnoreCase("Agregar")){
+            } else if (e.getActionCommand().equals("Aceptar")) {
+                if (frmEquipo.getAccion().equalsIgnoreCase("Agregar")) {
                     DtoEquipo dto = frmEquipo.obtenerDatos();
                     mdlEquipo.crear(dto.getNombreEq());
                     frmEquipo.visualizarMsg("Guardado correctamente");
                     frmEquipo.dispose();
                     pnlEquipo.VisualizarInformacion(mdlEquipo.buscarTodos());
-                }else if(frmEquipo.getAccion().equalsIgnoreCase("Editar")){                    
+                } else if (frmEquipo.getAccion().equalsIgnoreCase("Editar")) {
                     DtoEquipo dtoNuevo = frmEquipo.obtenerDatos();
                     DtoEquipo dtoActual = mdlEquipo.getDtoEquipoUpdate();
-                    if(mdlEquipo.actualizar(dtoActual,dtoNuevo)){
+                    if (mdlEquipo.actualizar(dtoActual, dtoNuevo)) {
                         frmEquipo.visualizarMsg("Guardado correctamente");
                         frmEquipo.dispose();
-                    }else{
+                        pnlEquipo.VisualizarInformacion(mdlEquipo.buscarTodos());
+                    } else {
                         frmEquipo.visualizarMsg("Ocurrió un error");
                         frmEquipo.dispose();
                     }
-                }else if(frmEquipo.getAccion().equalsIgnoreCase("Borrar")){                    
+                } else if (frmEquipo.getAccion().equalsIgnoreCase("Borrar")) {
                     DtoEquipo dto = frmEquipo.obtenerDatos();
-                    //DtoEquipo dtoDelete = mdlUsuario.buscarUsuario(dto.getUsuario());
-                    if(mdlEquipo.borrar(dto.getNombreEq())){
+                    if (mdlEquipo.borrar(dto.getNombreEq())) {
                         frmEquipo.visualizarMsg("Eliminado correctamente");
-                            frmEquipo.dispose();
-                    }else{
+                        frmEquipo.dispose();
+                        pnlEquipo.VisualizarInformacion(mdlEquipo.buscarTodos());
+                    } else {
                         frmEquipo.visualizarMsg("Ocurrió un error");
                         frmEquipo.dispose();
                     }
                 }
-            }else if(e.getActionCommand().equals("Cancelar")){
+            } else if (e.getActionCommand().equals("Cancelar")) {
                 frmEquipo.dispose();
             }
-        }catch (Exception ex) {
+        } catch (Exception ex) {
             JOptionPane.showMessageDialog(null, ex.getMessage());
         }
     }
-    
+
 }
